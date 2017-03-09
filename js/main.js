2902000c2556487913b19cf24c880df3af3458b5
Aaron Mohr <aaronjonmohr@gmail.com>
2017-03-09 01:50:26 -0500

    Site Update
    Additional JQuery animations

--- js/main.js
+++ js/main.js
@@ -4,34 +4,29 @@ $(document).ready(function() {
         
         $('.info-1 .proj-line').addClass('extend');
         $('.info ul li a').addClass('animated fadeInUp');
+        
     }
     
     else {
         $('.proj').removeClass('animated fadeInRight');
+        $('.proj-1,.proj-2,.proj-3,.proj-4').removeClass('animated zoomIn');
         $('.proj').addClass('animated fadeIn');
     }
     
     $('.info-1').hover(function(){
         
         if($(window).width() > 1024){
-
+            
+            $('.proj-1').css("display","block");
+            
+            $('.proj-2, .proj-3, .proj-4').css("display","none");
+            
             $(".info-1 ul li").css("color", "black");
             $(".info-1 ul li a").css("color", "black");
             $(".info-1 .proj-line").addClass("extend");
             $(".info-1 .proj-line").removeClass("descend");
-            $(".proj-1").css("display","block");
-
-            $(".info-2 ul li").css("color", "lightgrey");
-            $(".info-2 ul li a").css( "color", "lightgrey");
-            $(".proj-2").css("display","none");
-
-            $(".info-3 ul li").css( "color", "lightgrey" );
-            $(".info-3 ul li a").css( "color", "lightgrey");
-            $(".proj-3").css("display","none");
-
-            $(".info-4 ul li").css( "color", "lightgrey" );
-            $(".info-4 ul li a").css( "color", "lightgrey" );
-            $(".proj-4").css("display","none");
+            
+            $(".info-2 ul li, .info-2 ul li a, .info-3 ul li, .info-3 ul li a, .info-4 ul li, .info-4 ul li a").css( "color", "lightgrey" );
 
             if ($('.info-2 .proj-line').hasClass("extend")){
                $(".info-2 .proj-line").addClass("descend"); 
@@ -52,26 +47,19 @@ $(document).ready(function() {
     $('.info-2 li').hover(function(){
         
         if($(window).width() > 1024){
-
-            $(".info-1 ul li").css( "color", "lightgrey" );
-            $(".info-1 ul li a").css( "color", "lightgrey" );
-            $(".proj-1").css("display","none");
+            
+            $('.proj-1').addClass("animated zoomIn");
+            
+            $('.proj-1, .proj-3, .proj-4').css("display","none");
+            
+            $('.proj-2').css("display","block");
+            
+            $(".info-1 ul li, .info-1 ul li a, .info-3 ul li, .info-3 ul li a, .info-4 ul li, .info-4 ul li a").css( "color", "lightgrey" );
 
             $(".info-2 ul li").css( "color", "black" );
             $(".info-2 ul li a").css( "color", "black" );
             $(".info-2 .proj-line").addClass("extend");
             $(".info-2 .proj-line").removeClass("descend");
-            $(".proj-2").css("display","block");
-
-            $(".info-3 ul li").css( "color", "lightgrey" );
-            $(".info-3 ul li a").css( "color", "lightgrey" );
-            $(".proj-3").css("display","none");
-
-            $(".info-4 ul li").css( "color", "lightgrey" );
-            $(".info-4 ul li a").css( "color", "lightgrey" ); 
-            $(".proj-4").css("display","none");
-
-
 
             if ($('.info-1 .proj-line').hasClass("extend")){
                $(".info-1 .proj-line").addClass("descend"); 
@@ -92,24 +80,17 @@ $(document).ready(function() {
     $('.info-3 li').hover(function(){
         
         if($(window).width() > 1024){
-
-            $(".info-1 ul li").css( "color", "lightgrey" );
-            $(".info-1 ul li a").css( "color", "lightgrey" );
-            $(".proj-1").css("display","none");
-
-            $(".info-2 ul li").css( "color", "lightgrey" );
-            $(".info-2 ul li a").css( "color", "lightgrey" );
-            $(".proj-2").css("display","none");
-
+            
+            $('.proj-1, .proj-2, .proj-4').css("display","none");
+            
+            $('.proj-3').css("display","block");
+            
+            $(".info-1 ul li, .info-1 ul li a, .info-2 ul li, .info-2 ul li a, .info-4 ul li, .info-4 ul li a").css( "color", "lightgrey" );
+            
             $(".info-3 ul li").css( "color", "black" );
             $(".info-3 ul li a").css( "color", "black" );
             $(".info-3 .proj-line").addClass("extend");
             $(".info-3 .proj-line").removeClass("descend");
-            $(".proj-3").css("display","block");
-
-            $(".info-4 ul li").css( "color", "lightgrey" );
-            $(".info-4 ul li a").css( "color", "lightgrey" );
-            $(".proj-4").css("display","none");
 
             if ($('.info-1 .proj-line').hasClass("extend")){
                $(".info-1 .proj-line").addClass("descend"); 
@@ -130,24 +111,17 @@ $(document).ready(function() {
     $('.info-4 li').hover(function(){
         
         if($(window).width() > 1024){
-
-            $(".info-1 ul li").css( "color", "lightgrey" );
-            $(".info-1 ul li a").css( "color", "lightgrey" );
-            $(".proj-1").css("display","none");
-
-            $(".info-2 ul li").css( "color", "lightgrey" );
-            $(".info-2 ul li a").css( "color", "lightgrey" );
-            $(".proj-2").css("display","none");
-
-            $(".info-3 ul li").css( "color", "lightgrey" );
-            $(".info-3 ul li a").css( "color", "lightgrey" );
-            $(".proj-3").css("display","none");
+            
+            $('.proj-1, .proj-2, .proj-3').css("display","none");
+            
+            $('.proj-4').css("display","block");
+            
+            $(".info-1 ul li, .info-1 ul li a, .info-2 ul li, .info-2 ul li a, .info-3 ul li, .info-3 ul li a").css( "color", "lightgrey" );
 
             $(".info-4 ul li").css( "color", "black" );
             $(".info-4 ul li a").css( "color", "black" ); 
             $(".info-4 .proj-line").addClass("extend");
             $(".info-4 .proj-line").removeClass("descend");
-            $(".proj-4").css("display","block");
 
             if ($('.info-1 .proj-line').hasClass("extend")){
                $(".info-1 .proj-line").addClass("descend"); 
@@ -172,5 +146,6 @@ $(window).resize(function () {
     if (viewportWidth < 1024) {
         $('.proj-1').removeClass("animated fadeInRight");
         $('.proj-1, .proj-2, .proj-3, .proj-4').css("display","block");
+        $('.info ul li, .info ul li a').css("color","black");
     }
 });
